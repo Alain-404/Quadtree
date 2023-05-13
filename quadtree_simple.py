1362a23eda9cea2bf8aaa43ea000def38ddd1fc3
Alain Quispe <dequispech@est.unap.edu.pe>
2023-05-13 07:52:53 -0500

    Actualizacion_codigo
    Se agrego comentarios para facilitar su comprension

--- quadtree_simple.py
+++ quadtree_simple.py
@@ -1,60 +1,60 @@
 class QuadtreeNode:
-    def __init__(self, x, y, width, height):
+    #CONSTRUCTOR
+    def __init__(self, x, y, ancho, alto):
         self.x = x # coordenada x del nodo
         self.y = y # coordenada y del nodo
-        self.width = width # ancho del nodo
-        self.height = height # altura del nodo
-        self.points = [] # lista de puntos dentro del nodo
-        self.children = [None, None, None, None] # hijos del nodo
+        self.ancho = ancho # ancho del nodo
+        self.alto = alto # altura del nodo
+        self.puntos = [] # lista de puntos dentro del nodo
+        self.hijos = [None, None, None, None] # hijos del nodo
 
-    def insert(self, point):
+    def insert(self, punto):
         # Si el punto está dentro del nodo, lo agregamos a la lista de puntos
-        if self.x <= point[0] < self.x + self.width and self.y <= point[1] < self.y + self.height:
-            self.points.append(point)
+        if self.x <= punto[0] < self.x + self.ancho and self.y <= punto[1] < self.y + self.alto:
+            self.puntos.append(punto)
         else:
             # Si no está dentro del nodo, lo agregamos al hijo correspondiente
-            x = self.x + self.width/2
-            y = self.y + self.height/2
-            if point[0] < x:
-                if point[1] < y:
-                    if not self.children[0]:
-                        self.children[0] = QuadtreeNode(self.x, self.y, self.width/2, self.height/2)
-                    self.children[0].insert(point)
+            x = self.x + self.ancho/2
+            y = self.y + self.alto/2
+            if punto[0] < x:
+                if punto[1] < y:
+                    if not self.hijos[0]:
+                        self.hijos[0] = QuadtreeNode(self.x, self.y, self.ancho/2, self.alto/2)
+                    self.hijos[0].insert(punto)
                 else:
-                    if not self.children[1]:
-                        self.children[1] = QuadtreeNode(self.x, y, self.width/2, self.height/2)
-                    self.children[1].insert(point)
+                    if not self.hijos[1]:
+                        self.hijos[1] = QuadtreeNode(self.x, y, self.ancho/2, self.alto/2)
+                    self.hijos[1].insert(punto)
             else:
-                if point[1] < y:
-                    if not self.children[2]:
-                        self.children[2] = QuadtreeNode(x, self.y, self.width/2, self.height/2)
-                    self.children[2].insert(point)
+                if punto[1] < y:
+                    if not self.hijos[2]:
+                        self.hijos[2] = QuadtreeNode(x, self.y, self.ancho/2, self.alto/2)
+                    self.hijos[2].insert(punto)
                 else:
-                    if not self.children[3]:
-                        self.children[3] = QuadtreeNode(x, y, self.width/2, self.height/2)
-                    self.children[3].insert(point)
+                    if not self.hijos[3]:
+                        self.hijos[3] = QuadtreeNode(x, y, self.ancho/2, self.alto/2)
+                    self.hijos[3].insert(punto)
 
-    def query(self, x, y, width, height):
+    def query(self, x, y, ancho, alto):
         # Retorna todos los puntos que se encuentren dentro del area especificada
         result = []
-        for point in self.points:
-            if x <= point[0] < x + width and y <= point[1] < y + height:
-                result.append(point)
-        if self.children[0]:
-            result += self.children[0].query(x, y, width, height)
-        if self.children[1]:
-            result += self.children[1].query(x, y + self.height/2, width, height)
-        if self.children[2]:
-            result += self.children[2].query(x + self.width/2, y, width, height)
-        if self.children[3]:
-            result += self.children[3].query(x + self.width/2, y + self.height/2, width, height)
+        for punto in self.puntos:
+            if x <= punto[0] < x + ancho and y <= punto[1] < y + alto:
+                result.append(punto)
+        if self.hijos[0]:
+            result += self.hijos[0].query(x, y, ancho, alto)
+        if self.hijos[1]:
+            result += self.hijos[1].query(x, y + self.alto/2, ancho, alto)
+        if self.hijos[2]:
+            result += self.hijos[2].query(x + self.ancho/2, y, ancho, alto)
+        if self.hijos[3]:
+            result += self.hijos[3].query(x + self.ancho/2, y + self.alto/2, ancho, alto)
         return result
-    
 
 # Ejemplo de uso
-root = QuadtreeNode(0, 0, 100, 100)
-points = [(10, 10), (30, 40), (53, 50), (70, 30), (80, 80), (13, 19), (35, 36), (55, 52), (70, 39), (98, 85), (7, 14), (28, 33), (59, 45), (40, 15), (87, 96)]
-for point in points:
-    root.insert(point)
-result = root.query(0, 0, 50, 50)
-print(result)
+arbol = QuadtreeNode(0, 0, 100, 100)
+puntos = [(10, 10), (30, 40), (53, 50), (70, 30), (80, 80), (13, 19), (35, 36), (55, 52), (70, 39), (98, 85), (7, 14), (28, 33), (59, 45), (40, 15), (87, 96)]
+for punto in puntos:
+    arbol.insert(punto)
+result = arbol.query(0, 0, 50, 50)
+print("\n Los puntos en el espacio especificdo son: ", result)
